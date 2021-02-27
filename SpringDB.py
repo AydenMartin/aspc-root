8323bb923be3226ab0d9dcd80d54976e77c5a19e
tlhamilt <tlhamilt@mtu.edu>
2021-02-26 21:11:46 -0500

    Add keyword arguments

--- SpringDB.py
+++ SpringDB.py
@@ -28,21 +28,32 @@ def get_job(id):
 
 
 # insert a job into the table
-def insert_job(id, prog, scrap):
+def insert_job(id, **columns):
+    insert_cols = "id"
+    insert_values = str(id)
+    for k in columns.keys():
+        columns[k] = str(columns[k])
+    if len(columns) > 0:
+        insert_cols += ", " + ", ".join(columns.keys())
+        insert_values += ", " + ", ".join(columns.values())
     conn = sqlite3.connect(file_path)
     c = conn.cursor()
-    c.execute('''INSERT INTO jobs (id, prog, scrap) VALUES ({:d}, {:.2f}, {:.2f})'''.format(id, prog, scrap))
+    c.execute('''INSERT INTO jobs ({}) VALUES ({})'''.format(insert_cols, insert_values))
     conn.commit()
     conn.close()
 
 
 # update a job with the matching id and return the old row
-def update_job(id, prog, scrap):
+def update_job(id, **columns):
+    col_list = []
+    for k in columns.keys():
+        col_list.append(k + " = " + str(columns[k]))
+    update_str = ", ".join(col_list)
     conn = sqlite3.connect(file_path)
     c = conn.cursor()
     c.execute('''SELECT * FROM jobs WHERE id = {:d}'''.format(id))
     row = c.fetchone()
-    c.execute('''UPDATE jobs SET prog = {:.2f}, scrap = {:.2f} WHERE id = {:d}'''.format(prog, scrap, id))
+    c.execute('''UPDATE jobs SET {} WHERE id = {:d}'''.format(update_str, id))
     conn.commit()
     conn.close()
     return row
