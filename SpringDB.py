74d52d7c4b729b4c7161d519ccb641edeef8b383
tlhamilt <tlhamilt@mtu.edu>
2021-04-08 15:08:12 -0400

    Changed workcenter PK, added conversion methods

--- SpringDB.py
+++ SpringDB.py
@@ -2,12 +2,12 @@ import sqlite3
 import os
 
 col_dict = {"jobs": {"id": 0, "partid": 1, "capacity": 2, "completed": 3, "scrap": 4},
-           "workcenters": {"id": 0, "jobid": 1, "feed_wheel_diameter": 2, "output_toggle": 3, "threshold": 4,
-                           "state": 5, "rate": 6, "input_length": 7, "output_detected": 8,
-                           "finished_length_raw": 9, "time_started": 10, "time_ended": 11, "ip_address": 12},
-           "parts": {"id": 0, "part_length": 1, "part_num": 2, "part_type": 3, "part_bool": 4}}
+            "workcenters": {"id": 0, "workid": 1, "jobid": 2, "feed_wheel_diameter": 3, "output_toggle": 4,
+                            "threshold": 5, "state": 6, "rate": 7, "input_length": 8, "output_detected": 9,
+                            "finished_length_raw": 10, "time_started": 11, "time_ended": 12},
+            "parts": {"id": 0, "part_length": 1, "part_num": 2, "part_type": 3, "part_bool": 4}}
 tables = {0: "jobs", 1: "workcenters", 2: "parts"}
-bool_vals = {"parts": 4, "workcenters": 3}
+bool_vals = {"parts": 4, "workcenters": 4}
 
 
 class DataBase:
@@ -15,8 +15,8 @@ class DataBase:
     def __init__(self, file_path='C:\\Users\\tlham\\Documents\\spring.db'):
         if os.path.splitext(file_path)[1] != ".db":
             raise RuntimeError("Invalid file")
-        self.file_path = file_path
-        if os.path.exists(self.file_path):
+        self.__file_path = file_path
+        if os.path.exists(self.__file_path):
             self.__initialized = True
         else:
             self.__initialized = False
@@ -26,7 +26,7 @@ class DataBase:
         if self.__initialized:
             return RuntimeError('Db already initialized')
         try:
-            conn = sqlite3.connect(self.file_path)
+            conn = sqlite3.connect(self.__file_path)
         except sqlite3.Error as err:
             return err
         c = conn.cursor()
@@ -47,20 +47,20 @@ class DataBase:
             c.execute('''INSERT INTO jobs (id, partid, capacity) VALUES ('3', '1', 120)''')
             c.execute('''INSERT INTO jobs (id, partid, capacity, completed) VALUES ('4', '3', 25, 20)''')
 
-            c.execute('''CREATE TABLE workcenters (id varchar(30) PRIMARY KEY, jobid varchar(30), feed_wheel_diameter 
-            decimal(5, 2), output_toggle boolean DEFAULT 0 CHECK (output_toggle IN (0, 1)), threshold int DEFAULT 50, 
-            state int DEFAULT 0, rate decimal(5, 2) DEFAULT 0.0, input_length int, output_detected int, 
-            finished_length_raw int, time_started int, time_ended int, ip_address varchar(15), FOREIGN KEY (jobid) 
-            REFERENCES jobs (id))''')
-            c.execute('''INSERT INTO workcenters (id, state, feed_wheel_diameter, output_toggle) VALUES ('1', 0, 
+            c.execute('''CREATE TABLE workcenters (id varchar(15) PRIMARY KEY, workid varchar(30) UNIQUE, 
+            jobid varchar(30), feed_wheel_diameter decimal(5, 2), output_toggle boolean DEFAULT 0 CHECK (
+            output_toggle IN (0, 1)), threshold int DEFAULT 50, state int DEFAULT 0, rate decimal(5, 2) DEFAULT 0.0, 
+            input_length int, output_detected int, finished_length_raw int, time_started int, time_ended int, 
+            FOREIGN KEY (jobid) REFERENCES jobs (id))''')
+            c.execute('''INSERT INTO workcenters (id, state, feed_wheel_diameter, output_toggle) VALUES ('2.2.2.2', 0, 
             2.34, 1)''')
-            c.execute('''INSERT INTO workcenters (id, jobid, state, threshold) VALUES ('2', '1', 1, 45)''')
-            c.execute('''INSERT INTO workcenters (id, jobid, state, ip_address) VALUES ('3', '2', 0, '1.1.1.1')''')
-            c.execute('''INSERT INTO workcenters (id, state, output_toggle) VALUES ('4', 1, 1)''')
+            c.execute('''INSERT INTO workcenters (id, jobid, state, threshold) VALUES ('0.0.0.0', '1', 1, 45)''')
+            c.execute('''INSERT INTO workcenters (id, jobid, state) VALUES ('1.2.3.4', '2', 0)''')
+            c.execute('''INSERT INTO workcenters (id, workid, state, output_toggle) VALUES ('127.0.0.1', '4', 1, 1)''')
             conn.commit()
         except sqlite3.Error as err:
             conn.close()
-            os.remove(self.file_path)
+            os.remove(self.__file_path)
             return err
         conn.close()
         self.__initialized = True
@@ -80,7 +80,7 @@ class DataBase:
         if table not in tables.values():
             return None, RuntimeError("Invalid table")
         try:
-            conn = sqlite3.connect(self.file_path)
+            conn = sqlite3.connect(self.__file_path)
         except sqlite3.Error as err:
             return None, err
         c = conn.cursor()
@@ -114,7 +114,7 @@ class DataBase:
             insert_cols += ", " + k
             insert_values += ", ?"
         try:
-            conn = sqlite3.connect(self.file_path)
+            conn = sqlite3.connect(self.__file_path)
         except sqlite3.Error as err:
             return err
         c = conn.cursor()
@@ -144,7 +144,7 @@ class DataBase:
             col_list.append(k + " = ?")
         update_str = ", ".join(col_list)
         try:
-            conn = sqlite3.connect(self.file_path)
+            conn = sqlite3.connect(self.__file_path)
         except sqlite3.Error as err:
             return None, err
         c = conn.cursor()
@@ -174,7 +174,7 @@ class DataBase:
         if table not in tables.values():
             return None, RuntimeError("Invalid table")
         try:
-            conn = sqlite3.connect(self.file_path)
+            conn = sqlite3.connect(self.__file_path)
         except sqlite3.Error as err:
             return None, err
         c = conn.cursor()
@@ -194,17 +194,20 @@ class DataBase:
         conn.close()
         return row, None
 
-    def clear_tables(self):
+    def clear_table(self, table):
         if not self.__initialized:
             return RuntimeError("DB not initialized")
+        if type(table) == int and table < len(tables):
+            table = tables[table]
+        if table not in tables.values():
+            return None, RuntimeError("Invalid table")
         try:
-            conn = sqlite3.connect(self.file_path)
+            conn = sqlite3.connect(self.__file_path)
         except sqlite3.Error as err:
             return err
         c = conn.cursor()
         try:
-            for t in tables:
-                c.execute("DELETE FROM {}".format(tables[t]))
+            c.execute("DELETE FROM {}".format(table))
             conn.commit()
         except sqlite3.Error as err:
             conn.close()
@@ -220,7 +223,7 @@ class DataBase:
         if table not in tables.values():
             return None, RuntimeError("Invalid table")
         try:
-            conn = sqlite3.connect(self.file_path)
+            conn = sqlite3.connect(self.__file_path)
         except sqlite3.Error as err:
             return None, err
         c = conn.cursor()
@@ -232,3 +235,41 @@ class DataBase:
             return None, err
         conn.close()
         return count != 0, None
+
+    def workcenter_addr_to_id(self, addr):
+        if not self.__initialized:
+            return None, RuntimeError("DB not initialized")
+        try:
+            conn = sqlite3.connect(self.__file_path)
+        except sqlite3.Error as err:
+            return None, err
+        c = conn.cursor()
+        try:
+            c.execute("SELECT workid FROM workcenters WHERE id = ?", (addr,))
+            row = c.fetchone()
+            if row is None:
+                return None, None
+        except sqlite3.Error as err:
+            conn.close()
+            return None, err
+        conn.close()
+        return row[0], None
+
+    def workcenter_id_to_addr(self, workid):
+        if not self.__initialized:
+            return None, RuntimeError("DB not initialized")
+        try:
+            conn = sqlite3.connect(self.__file_path)
+        except sqlite3.Error as err:
+            return None, err
+        c = conn.cursor()
+        try:
+            c.execute("SELECT id FROM workcenters WHERE workid = ?", (workid,))
+            row = c.fetchone()
+            if row is None:
+                return None, None
+        except sqlite3.Error as err:
+            conn.close()
+            return None, err
+        conn.close()
+        return row[0], None
